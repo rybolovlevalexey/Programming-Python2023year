0ec98dc3db09916820e6dca99661c421951f512a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-01-12 20:39:19 +0300

    task number 3

--- Cracking Coding Interview/Arrays and strings.py
+++ Cracking Coding Interview/Arrays and strings.py
@@ -1,20 +1,15 @@
-st1 = input()
-st2 = input()
-if len(st1) != len(st2):
-    print("NO")
-else:
-    letters = dict()
-    for i in range(len(st1)):
-        if st1[i] in letters:
-            letters[st1[i]] += 1
-        else:
-            letters[st1[i]] = 1
-
-        if st2[i] in letters:
-            letters[st2[i]] -= 1
-        else:
-            letters[st2[i]] = -1
-    if len(set(letters.values())) == 1:
-        print("YES")
+st = input().lower()
+letters = dict()
+for i in range(len(st)):
+    elem = st[i]
+    if not elem.isalpha():
+        continue
+    if elem in letters:
+        letters[elem] += 1
     else:
-        print("NO")
+        letters[elem] = 1
+res = len(list(filter(lambda x: x % 2 == 1, letters.values())))
+if res <= 1:
+    print("YES")
+else:
+    print("NO")
