a93889f7fbb1d531f4f5fe2a79a98b3d8a58701c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-01-12 20:26:08 +0300

    task number 2

--- Cracking Coding Interview/Arrays and strings.py
+++ Cracking Coding Interview/Arrays and strings.py
@@ -1,13 +1,20 @@
-st = input()  # original string
-letters = ""
-flag = True
-for elem in st:
-    if elem in letters:
-        flag = False
-        break
-    else:
-        letters += elem
-if flag:
-    print("All letters in string do not repeat")
+st1 = input()
+st2 = input()
+if len(st1) != len(st2):
+    print("NO")
 else:
-    print("There is letter, which repeat")
+    letters = dict()
+    for i in range(len(st1)):
+        if st1[i] in letters:
+            letters[st1[i]] += 1
+        else:
+            letters[st1[i]] = 1
+
+        if st2[i] in letters:
+            letters[st2[i]] -= 1
+        else:
+            letters[st2[i]] = -1
+    if len(set(letters.values())) == 1:
+        print("YES")
+    else:
+        print("NO")
