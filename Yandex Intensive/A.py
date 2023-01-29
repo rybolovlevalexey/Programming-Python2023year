8f4425dbff6bdd4647e5fa9dc3fb0e28bba69200
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-01-29 12:34:43 +0500

    done

--- Yandex Intensive/A.py
+++ Yandex Intensive/A.py
@@ -1,30 +1,14 @@
 st = input()
 sp = list(map(int, st.split()))
-if "-" not in st:
-    mn1 = None
-    mn2 = None
-    for elem in sp:
-        if mn1 is None:
-            mn1 = elem
-        elif mn2 is None:
-            mn2 = elem
-        else:
-            if elem < mn1:
-                mn1, mn2 = elem, mn1
-            elif mn1 < elem < mn2:
-                mn2 = elem
-    print(mn1 * mn2)
-else:
-    minus = None
-    plus = None
-    for elem in sp:
-        if minus is None and elem < 0:
-            minus = elem
-        if plus is None and elem > 0:
-            plus = elem
-        else:
-            if elem > plus and elem > 0:
-                plus = elem
-            if elem < minus and elem < 0:
-                plus = elem
-    print(plus * minus)
+ans = None
+start = 0
+end = start + 1
+while end < len(sp):
+    res = sp[start] * sp[end]
+    if ans is None or res < ans:
+        ans = res
+    end += 1
+    if end == len(sp):
+        start += 1
+        end = start + 1
+print(ans)
