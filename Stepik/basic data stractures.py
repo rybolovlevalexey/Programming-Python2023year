2ea3da0c9cc25f22fc71961e3979d514d16e17dc
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-01 21:47:42 +0500

    done

--- Stepik/basic data stractures.py
+++ Stepik/basic data stractures.py
@@ -1,7 +1,6 @@
 n = int(input())
 sp = list(map(int, input().split()))
-k, x = map(int, input().split())
-for i in range(k, n):
-    sp[i], x = x, sp[i]
-sp.append(x)
-print(*sp)
+k = int(input())
+for i in range(k, n - 1):
+    sp[i] = sp[i + 1]
+print(*sp[:-1])
