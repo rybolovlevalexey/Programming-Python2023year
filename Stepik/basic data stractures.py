f87702d2649bdd7c8f5865061d9dcf2339aac7b3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-01 22:12:57 +0500

    done

--- Stepik/basic data stractures.py
+++ Stepik/basic data stractures.py
@@ -2,17 +2,28 @@ n = int(input())
 sp1 = list(map(int, input().split()))
 m = int(input())
 sp2 = list(map(int, input().split()))
+ans = list()
 i = 0
 j = 0
 while i < n and j < m:
-    if sp1[i] == sp2[j]:
+    if sp1[i] < sp2[j]:
+        ans.append(sp1[i])
         print(sp1[i], end=" ")
+        i += 1
+    elif sp1[i] == sp2[j]:
+        ans.append(sp1[i])
+        ans.append(sp2[j])
+        print(sp1[i], end=" ")
+        print(sp2[j], end=" ")
         i += 1
         j += 1
     else:
-        if sp1[i] > sp2[j]:
-            while j < m and sp1[i] > sp2[j]:
-                j += 1
-        else:
-            while i < n and sp1[i] < sp2[j]:
-                i += 1
+        ans.append(sp2[j])
+        print(sp2[j], end=" ")
+        j += 1
+while i < n:
+    print(sp1[i], end=" ")
+    i += 1
+while j < m:
+    print(sp2[j], end=" ")
+    j += 1
