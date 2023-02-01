821ba7d29d87362a031ce3b7255401eb1c1b5d4b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-01 22:26:36 +0500

    done

--- Stepik/basic data stractures.py
+++ Stepik/basic data stractures.py
@@ -1,29 +1,31 @@
+def checking(spisok):
+    for i in range(len(spisok) - 2):
+        if spisok[i] == spisok[i + 1] == spisok[i + 2]:
+            return True
+    return False
+
+
+ans = 0
 n = int(input())
-sp1 = list(map(int, input().split()))
-m = int(input())
-sp2 = list(map(int, input().split()))
-ans = list()
-i = 0
-j = 0
-while i < n and j < m:
-    if sp1[i] < sp2[j]:
-        ans.append(sp1[i])
-        print(sp1[i], end=" ")
-        i += 1
-    elif sp1[i] == sp2[j]:
-        ans.append(sp1[i])
-        ans.append(sp2[j])
-        print(sp1[i], end=" ")
-        print(sp2[j], end=" ")
-        i += 1
-        j += 1
-    else:
-        ans.append(sp2[j])
-        print(sp2[j], end=" ")
-        j += 1
-while i < n:
-    print(sp1[i], end=" ")
-    i += 1
-while j < m:
-    print(sp2[j], end=" ")
-    j += 1
+sp = list(map(int, input().split()))
+while checking(sp):
+    print(sp)
+    i = 0
+    mem = None
+    while i + 2 < len(sp):
+        if mem is None and sp[i] == sp[i + 1] == sp[i + 2]:
+            mem = sp[i]
+            del sp[i]
+            ans += 1
+        elif not mem is None and mem == sp[i] == sp[i + 1] and sp[i + 1] != sp[i + 2]:
+            del sp[i]
+            del sp[i]
+            ans += 2
+            mem = None
+        elif sp[i] != sp[i + 1]:
+            i += 1
+    if not mem is None and mem == sp[i] == sp[i + 1] and i + 2 == len(sp):
+        del sp[i]
+        del sp[i]
+        ans += 2
+print(ans)
