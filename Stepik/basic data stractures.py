b1fa7de781503f72822eea55ffafb4191d562120
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-02 21:43:28 +0500

    done

--- Stepik/basic data stractures.py
+++ Stepik/basic data stractures.py
@@ -1,2 +1,7 @@
 n = int(input())
-print(sum(filter(lambda x: x % 2 == 0 and x > 0, map(int, input().split()))))
+sp = list(map(int, input().split()))
+ans = 0
+for i in range(1, n, 2):
+    if sp[i] > 0:
+        ans += sp[i]
+print(ans)
