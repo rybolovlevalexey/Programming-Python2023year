2200ff9d42d2b2d5617374a729faf281d9e53d44
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-18 21:02:58 +0300

    push, pop, back

--- Training on algorithms from yandex/11.py
+++ Training on algorithms from yandex/11.py
@@ -2,4 +2,20 @@ class Stack:
     def __init__(self):
         self.sp = list()
 
-    
+    def push(self, elem):
+        self.sp.append(elem)
+        print("ok")
+
+    def pop(self):
+        if len(self.sp) == 0:
+            print("error")
+        else:
+            x = self.sp[-1]
+            del self.sp[-1]
+            print(x)
+
+    def back(self):
+        if len(self.sp) == 0:
+            print("error")
+        else:
+            print(self.sp[-1])
