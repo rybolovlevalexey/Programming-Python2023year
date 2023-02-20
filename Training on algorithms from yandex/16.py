63d0fb5ab0fac1beab2e37462725158fea230ac9
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-20 19:21:49 +0300

    В Queue добавлены str, push, pop

--- Training on algorithms from yandex/16.py
+++ Training on algorithms from yandex/16.py
@@ -1,22 +1,64 @@
 class QueueElement:
     def __init__(self, x):
-        self.value = x
-        self.Prev = None
-        self.Next = None
+        self.Value = x
+        self.__Prev = None
+        self.__Next = None
 
     @property
-    def prev(self):
-        return self.Prev
+    def Prev(self):
+        return self.__Prev
 
-    @prev.setter
-    def prev(self, elem):
-        self.Prev = elem
+    @Prev.setter
+    def Prev(self, elem):
+        self.__Prev = elem
 
     @property
-    def next(self):
-        return self.Next
+    def Next(self):
+        return self.__Next
 
-    @next.setter
-    def next(self, elem):
-        self.Next = elem
+    @Next.setter
+    def Next(self, elem):
+        self.__Next = elem
 
+
+class Queue:
+    def __init__(self):
+        self.Head = None
+
+    def __str__(self):
+        cur = self.Head
+        if cur is None:
+            return "nothing"
+        st = str(cur.Value) + "; "
+        while cur is not None:
+            cur = cur.Next
+            if cur is not None:
+                st += (str(cur.Value) + "; ")
+        return st[:-2]
+
+    def push(self, value):
+        if self.Head is None:
+            self.Head = QueueElement(value)
+        else:
+            cur = self.Head
+            while cur.Next is not None:
+                cur = cur.Next
+            cur.Next = QueueElement(value)
+            cur_new = cur.Next
+            cur_new.Prev = cur
+        print("ok")
+
+    def pop(self):
+        if self.Head is None:
+            print("error")
+        else:
+            val = self.Head.Value
+            self.Head = self.Head.Next
+            self.Head.Prev = None
+            print(val)
+
+
+queue = Queue()
+queue.push(1)
+queue.push(2)
+queue.push(3)
