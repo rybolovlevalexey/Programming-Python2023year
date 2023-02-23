4709af1ff095b370ea6380f563f630527727acf2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-23 14:19:04 +0300

    Коллекционер Диего

--- Training on algorithms from yandex/Разминка/3.py
+++ Training on algorithms from yandex/Разминка/3.py
@@ -1,11 +1,46 @@
-n = int(input())
-diego = set(sorted(map(int, input().split())))
+def func(pi):
+    global n
+    end = n
+    while True:
+        if end == 1:
+            if diego[end] < pi:
+                return 1
+            else:
+                if diego[0] < pi:
+                    return 0
+                return -1
+        ind = end // 2
+        elem = diego[ind]
+        if elem == pi:
+            #print("here 11")
+            return ind - 1
+        if elem > pi and diego[ind - 1] < pi:
+            #print("here12")
+            return ind - 1
+        if elem < pi:
+            end = (end + ind) // 2
+        else:
+            end = ind
+
+
+int(input())
+diego = tuple(set(map(int, input().split())))
+mx = max(diego)
+mn = min(diego)
+n = len(diego)
 k = int(input())
-collectors = list(map(int, input().split()))
-for p in collectors:
-    ans = 0
-    for elem in diego:
-        if elem >= p:
-            break
-        ans += 1
-    print(ans)
+
+for collector in map(int, input().split()):
+    if n == 1:
+        print(1 if diego[0] < collector else 0)
+    elif mx < collector:
+        #print("here1", end=' ')
+        print(n)
+    elif mx == collector:
+        print(n - 1)
+    elif mn >= collector:
+        #print("here2", end=' ')
+        print(0)
+    else:
+        #print("here3", end=' ')
+        print(func(collector) + 1)
