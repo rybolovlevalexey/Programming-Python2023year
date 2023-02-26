d4d824260759d665250f5d5c6eaa9a8ecbdda8a2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-26 22:06:48 +0300

    Тимур и книги

--- Stepik/basic data stractures.py
+++ Stepik/basic data stractures.py
@@ -1,18 +1,9 @@
 n = int(input())
-sp1 = set()
+books = set()
 for i in range(n):
-    sp1.add(input())
-m = int(input())
-cnt = 0
-sp2 = set()
-for j in range(m):
-    st = input()
-    if st in sp1:
-        sp1.remove(st)
+    books.add(input())
+for i in range(int(input())):
+    if input() in books:
+        print("YES")
     else:
-        sp2.add(st)
-cnt = len(sp1) + len(sp2)
-if cnt == 0:
-    print("NO")
-else:
-    print(cnt)
+        print("NO")
