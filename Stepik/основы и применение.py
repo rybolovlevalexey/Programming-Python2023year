086c402e5d017b4f4a6dea1aef77bba31f5a151e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-02-28 09:33:48 +0300

    проверка наследования классов - прошёл все тесты без обхода графов

--- Stepik/основы и применение.py
+++ Stepik/основы и применение.py
@@ -4,14 +4,20 @@ n = int(input())
 for i in range(n):
     stroka = input().strip()
     if ":" not in stroka:
-        sp_classes[stroka] = []
+        sp_classes[stroka] = set()
     else:
         name, parents = stroka.split(" : ")
-        sp_classes[name] = parents.split()
-        for par in parents:
-            sp_classes[name].extend(sp_classes.get(par, []))
+        sp_classes[name] = set(parents.split())
+for key in sp_classes.keys():
+    stack = list(sp_classes[key])
+    while len(stack) > 0:
+        par = stack.pop(0)
+        sp_classes[key].add(par)
+        for new_par in sp_classes[par]:
+            if new_par not in stack and new_par not in sp_classes[key]:
+                stack.append(new_par)
+#print(sp_classes)
 
-print(sp_classes)
 m = int(input())
 for i in range(m):
     first, second = input().split()
