e1b510e86e930b70d15e2ed5587de8f51252665e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-03 17:45:19 +0300

    калькулятор - ML

--- Training on algorithms from yandex/Динамическое программирование/23.py
+++ Training on algorithms from yandex/Динамическое программирование/23.py
@@ -1,29 +1,40 @@
-class TreeNode:
-    def __init__(self, name):
-        self.name = name
-        self.first = None
-        self.second = None
-        self.third = None
-
-
-class Tree:
-    def __init__(self, name):
-        self.Head = TreeNode(name)
-
-
+# индекс - соответствующее число
 n = int(input())
-prev = [None]
-dp = [0] * n
-dp[0] = 1
-for i in range(1, n):
-    prev1 = set()
-    if i - 1 >= 0:
-        dp[i] += dp[i - 1]
-        prev1.add(i)
-    if (i + 1) % 2 == 0:
-        dp[i] += dp[i // 2]
-        prev1.add((i + 1) // 2)
-    if (i + 1) % 3 == 0:
-        dp[i] += dp[i // 3]
-        prev1.add((i + 1) // 3)
-    prev.append(list(prev1))
+dp = list([0, 0, 0] for i in range(n + 1))
+dp[1] = [1, 0, 0]  # (кол-во способов, номер операции, из какого числа пришёл)
+for i in range(2, n + 1):
+    dp[i][0] += dp[i - 1][0]
+    if i % 2 != 0 and i % 3 != 0:
+        dp[i][1] = dp[i - 1][1] + 1
+        dp[i][2] = i - 1
+    if i % 2 == 0 and i % 3 != 0:
+        dp[i][0] += dp[i // 2][0]
+        dp[i][1] = min(dp[i - 1][1], dp[i // 2][1]) + 1
+        if dp[i - 1][1] < dp[i // 2][1]:
+            dp[i][2] = i - 1
+        else:
+            dp[i][2] = i // 2
+    if i % 3 == 0 and i % 2 != 0:
+        dp[i][0] += dp[i // 3][0]
+        dp[i][1] = min(dp[i - 1][1], dp[i // 3][1]) + 1
+        if dp[i - 1][1] < dp[i // 3][1]:
+            dp[i][2] = i - 1
+        else:
+            dp[i][2] = i // 3
+    if i % 3 == 0 and i % 2 == 0:
+        dp[i][0] += (dp[i // 2][0] + dp[i // 3][0])
+        dp[i][1] = min(dp[i - 1][1], dp[i // 3][1], dp[i // 2][1]) + 1
+        if dp[i - 1][1] < dp[i // 2][1] and dp[i - 1][1] < dp[i // 3][1]:
+            dp[i][2] = i - 1
+        elif dp[i // 2][1] < dp[i // 3][1]:
+            dp[i][2] = i // 2
+        else:
+            dp[i][2] = i // 3
+
+print(dp[-1][1])
+ans = ""
+k = n
+while k > 0:
+    ans += str(k) + " "
+    k = dp[k][2]
+print(ans.strip()[::-1])
