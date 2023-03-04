858cb48ad7cb69e31712b02e2bf1b1f469cc91a2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-04 13:36:45 +0300

    НОП с восстановлением ответа

--- Training on algorithms from yandex/динамическое программирование с двумя параметрами/30.py
+++ Training on algorithms from yandex/динамическое программирование с двумя параметрами/30.py
@@ -1,37 +1,50 @@
 n = int(input())
-st1 = input().split()
+st1 = input().split()  # j
 m = int(input())
-st2 = input().split()
-result = list([0] * n for i in range(m))
-flag = False
-for i in range(n):
-    if flag or st1[i] == st2[0]:
-        flag = True
-        result[0][i] = 1
-    else:
-        result[0][i] = 0
-flag = False
-for j in range(m):
-    if flag or st2[j] == st1[0]:
-        flag = True
-        result[j][0] = 1
-    else:
-        result[j][0] = 0
-for i in range(1, n):
-    flag = False
-    for j in range(1, m):
-        if max(result[i - 1][j], result[i][j - 1]) == i + 1 or \
-                max(result[i - 1][j], result[i][j - 1]) == j + 1:
-            result[i][j] = max(result[i - 1][j], result[i][j - 1])
-        else:
-            if flag:
-                result[i][j] = i + 1
+st2 = input().split()  # i
+dp = list([0] * n for i in range(m))
+ans = ""
+for i in range(m):
+    elem = st2[i]
+    for j in range(n):
+        if i == 0 and j == 0:
+            if st1[j] == st2[i]:
+                dp[i][j] = 1
             else:
-                if st1[j] == st2[i]:
-                    result[i][j] = max(result[i - 1][j], result[i][j - 1]) + 1
-                    if result[i][j] == i + 1:
-                        flag = True
+                dp[i][j] = 0
+        else:
+            if elem == st1[j]:
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
                 else:
-                    result[i][j] = max(result[i - 1][j], result[i][j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                if i == 0:
+                    dp[i][j] = dp[i][j - 1]
+                elif j == 0:
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+i = m - 1
+j = n - 1
+#print(*dp, sep="\n")
+while i >= 0 and j >= 0:
+    if i == 0 and j == 0 and dp[i][j] == 1:
+        ans += st1[j]
 
-print(*result, sep="\n")
+    if j == 0 and i > 0 and dp[i - 1][j] < dp[i][j]:
+        ans += st1[j][::-1] + " "
+        j -= 1
+    elif (dp[i - 1][j] < dp[i][j] and dp[i][j - 1] < dp[i][j]) or \
+            (i == 0 and dp[i][j - 1] < dp[i][j]):
+        ans += st1[j][::-1] + " "
+        i -= 1
+        j -= 1
+    elif dp[i][j - 1] < dp[i][j] and dp[i - 1][j] == dp[i][j]:
+        i -= 1
+    else:
+        j -= 1
+        if j < 0:
+            j = n - 1
+            i -= 1
+print(ans[::-1].strip())
