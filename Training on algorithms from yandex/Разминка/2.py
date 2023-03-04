969c8d12a039a433ab552c7dceafda95f34f9aa8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-04 11:12:42 +0300

    Красивая строка - TL

--- Training on algorithms from yandex/Разминка/2.py
+++ Training on algorithms from yandex/Разминка/2.py
@@ -1,35 +1,117 @@
 k = int(input())
 st = input()
-ans = tuple()
-
-for letter in set(st):
-    end = 0
-    cnt = k
-    for beg in range(len(st)):
-        if st[beg] != letter and cnt > 0:
-            cnt -= 1
-        if beg != 0 and st[beg - 1] != letter:
-            cnt += 1
-
-        while end < len(st):
-            if end <= beg:
-                pass
+if k == 0:
+    dl = mx_dl = 0
+    last = None
+    for elem in st:
+        if last is None:
+            last = elem
+            dl += 1
+            if dl > mx_dl:
+                mx_dl = dl
+        elif last == elem:
+            dl += 1
+            if dl > mx_dl:
+                mx_dl = dl
+        elif last != elem:
+            last = elem
+            dl = 1
+    print(dl)
+else:
+    ans = None
+    for let in range(ord("a"), ord("z") + 1):
+        left = 0
+        right = 1
+        elem = chr(let)
+        cnt = k
+        mx_dl = right - left + 1
+        if st[left] != elem and st[right] != elem and cnt >= 2:  # дописать
+            cnt -= 2
+            right += 1
+            while left < len(st):
+                while right < len(st):
+                    if st[right] != elem and cnt == 0:
+                        break
+                    dl = right - left + 1
+                    if st[right] == elem:
+                        if dl > mx_dl:
+                            mx_dl = dl
+                    elif st[right] != elem and cnt > 0:
+                        cnt -= 1
+                        if dl > mx_dl:
+                            mx_dl = dl
+                    right += 1
+                left += 1
+                if st[left - 1] != elem:
+                    cnt += 1
+        elif st[left] != elem and st[right] != elem and cnt < 2:
+            while st[left] != elem and st[right] != elem and right < len(st):
+                left += 1
+                right += 1
+            if right + 1 == len(st):
+                mx_dl = 1  # выход
             else:
-                if st[end] == letter and cnt == 0:
-                    dl = end - beg + 1
-                    if len(ans) == 0 or dl > ans[0]:
-                        ans = (dl, st[beg: end + 1])
-                elif st[end] != letter and cnt == 0:
-                    break
-                elif st[end] != letter and cnt > 0:
+                if (st[left] != elem and st[right] == elem) or \
+                        (st[left] == elem and st[right] != elem):
                     cnt -= 1
-                    dl = end - beg + 1
-                    if len(ans) == 0 or dl > ans[0]:
-                        ans = (dl, st[beg: end + 1])
-                elif st[end] == letter and cnt > 0:
-                    # цикл продолжается, если буква совпала
-                    dl = end - beg + 1
-                    if len(ans) == 0 or dl > ans[0]:
-                        ans = (dl, st[beg: end + 1])
-            end += 1
-print(ans[0])
+                    right += 1
+                    while left < len(st):
+                        while right < len(st):
+                            if st[right] != elem and cnt == 0:
+                                break
+                            dl = right - left + 1
+                            if st[right] == elem:
+                                if dl > mx_dl:
+                                    mx_dl = dl
+                            elif st[right] != elem and cnt > 0:
+                                cnt -= 1
+                                if dl > mx_dl:
+                                    mx_dl = dl
+                            right += 1
+                        left += 1
+                        if st[left - 1] != elem:
+                            cnt += 1
+
+        elif (st[left] != elem and st[right] == elem) or \
+                (st[left] == elem and st[right] != elem):  # дописать
+            cnt -= 1
+            right += 1
+            while left < len(st):
+                while right < len(st):
+                    if st[right] != elem and cnt == 0:
+                        break
+                    dl = right - left + 1
+                    if st[right] == elem:
+                        if dl > mx_dl:
+                            mx_dl = dl
+                    elif st[right] != elem and cnt > 0:
+                        cnt -= 1
+                        if dl > mx_dl:
+                            mx_dl = dl
+                    right += 1
+                left += 1
+                if st[left - 1] != elem:
+                    cnt += 1
+
+        elif st[right] == st[left] == elem:
+            # цикл работает корректно
+            while left < len(st):
+                while right < len(st):
+                    if st[right] != elem and cnt == 0:
+                        break
+                    dl = right - left + 1
+                    if st[right] == elem:
+                        if dl > mx_dl:
+                            mx_dl = dl
+                    elif st[right] != elem and cnt > 0:
+                        cnt -= 1
+                        if dl > mx_dl:
+                            mx_dl = dl
+                    right += 1
+                left += 1
+                if st[left - 1] != elem:
+                    cnt += 1
+
+        if ans is None or ans < mx_dl:
+            ans = mx_dl
+    print(ans)
