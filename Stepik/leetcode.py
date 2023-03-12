63193ace7165e805ec9647bf586b0caf77aec5a2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-12 10:55:49 +0300

    Categorize the box

--- Stepik/leetcode.py
+++ Stepik/leetcode.py
@@ -1,26 +1,23 @@
 class Solution(object):
-    def fizzBuzz(self, n):
+    def categorizeBox(self, length, width, height, mass):
         """
-        :type n: int
-        :rtype: List[str]
+        :type length: int
+        :type width: int
+        :type height: int
+        :type mass: int
+        :rtype: str
         """
-        ans = list()
-        three, five, num = 1, 1, 1
-        while num <= n:
-            if five == 5 and three == 3:
-                ans.append("FizzBuzz")
-                five, three = 1, 1
-            elif three == 3:
-                ans.append("Fizz")
-                three = 1
-                five += 1
-            elif five == 5:
-                ans.append("Buzz")
-                five = 1
-                three += 1
-            else:
-                ans.append(str(num))
-                three += 1
-                five += 1
-            num += 1
-        return ans
+
+        flag1 = (length >= 10**4 or width >= 10**4 or height >= 10**4 or (width * height * length) >= 10**9)
+        flag2 = mass >= 100
+        if flag1 and flag2:
+            return "Both"
+        if not flag1 and not flag2:
+            return "Neither"
+        if flag1:
+            return "Bulky"
+        return "Heavy"
+
+sl = Solution()
+
+print(sl.categorizeBox(2593, 6432, 46, 412))
