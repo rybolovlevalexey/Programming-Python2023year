40b817572268691f5050e1e00a747de5e359af3b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-12 12:06:05 +0300

    поиск информации с условиями в csv

--- Stepik/регулярные выражения.py
+++ Stepik/регулярные выражения.py
@@ -1,8 +1,13 @@
-import re
-import sys
+import csv
 
-
-for line in sys.stdin:
-    line = line.strip()
-    res = re.sub(r"\b(\w{1})(\w{1})(\w*)\b", "\\2\\1\\3", line)
-    print(res)
+file = open("Crimes.csv", "r")
+ans_d = dict()
+cfile = csv.DictReader(file)
+for row in cfile:
+    if row["Date"].split()[0].split("/")[2] != "2015":
+        continue
+    pr_type = row["Primary Type"]
+    ans_d[pr_type] = ans_d.get(pr_type, 0) + 1
+for key in ans_d.keys():
+    if ans_d[key] == max(ans_d.values()):
+        print(key)
