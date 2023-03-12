0ff66cf1ef31afb196f29ea2ca0b740dd6fe91fa
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-12 11:42:38 +0300

    поменять местами первые две буквы

--- Stepik/leetcode.py
+++ Stepik/leetcode.py
@@ -1,23 +1,59 @@
-class Solution(object):
-    def categorizeBox(self, length, width, height, mass):
+class FizzBuzz(object):
+    def __init__(self, n):
+        self.n = n
+        for num in range(1, n + 1):
+            if num % 3 == 0 and num % 5 == 0:
+                self.fizzbuzz(self.printFizzBuzz)
+            elif num % 3 == 0:
+                self.fizz(self.printFizz)
+            elif num % 5 == 0:
+                self.buzz(self.printBuzz)
+            else:
+                self.number(num, self.prNumber)
+
+    def prNumber(self, x):
+        print(str(x))
+
+    def printFizzBuzz(self):
+        print("fizzbuzz")
+
+    def printBuzz(self):
+        print("buzz")
+
+    def printFizz(self):
+        print("fizz")
+
+    # printFizz() outputs "fizz"
+    def fizz(self, printFizz):
         """
-        :type length: int
-        :type width: int
-        :type height: int
-        :type mass: int
-        :rtype: str
+        :type printFizz: method
+        :rtype: void
         """
+        printFizz()
 
-        flag1 = (length >= 10**4 or width >= 10**4 or height >= 10**4 or (width * height * length) >= 10**9)
-        flag2 = mass >= 100
-        if flag1 and flag2:
-            return "Both"
-        if not flag1 and not flag2:
-            return "Neither"
-        if flag1:
-            return "Bulky"
-        return "Heavy"
+    # printBuzz() outputs "buzz"
+    def buzz(self, printBuzz):
+        """
+        :type printBuzz: method
+        :rtype: void
+        """
+        printBuzz()
 
-sl = Solution()
+    # printFizzBuzz() outputs "fizzbuzz"
+    def fizzbuzz(self, printFizzBuzz):
+        """
+        :type printFizzBuzz: method
+        :rtype: void
+        """
+        printFizzBuzz()
 
-print(sl.categorizeBox(2593, 6432, 46, 412))
+    # printNumber(x) outputs "x", where x is an integer.
+    def number(self, x, printNumber):
+        """
+        :type printNumber: method
+        :rtype: void
+        """
+        printNumber(x)
+
+
+sl = FizzBuzz(15)
--- Stepik/регулярные выражения.py
+++ Stepik/регулярные выражения.py
@@ -4,5 +4,5 @@ import sys
 
 for line in sys.stdin:
     line = line.strip()
-    res = re.sub(r"(\w{2,})", "\\1", line)
+    res = re.sub(r"\b(\w{1})(\w{1})(\w*)\b", "\\2\\1\\3", line)
     print(res)
