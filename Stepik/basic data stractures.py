982ebdd6a2f888301955d7f30c4b7dd667e0e8ba
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-14 21:39:17 +0300

    заполнение диагоналями справа налево

--- Stepik/basic data stractures.py
+++ Stepik/basic data stractures.py
@@ -1,8 +1,24 @@
-ans = dict()
-for i in range(int(input())):
-    st = input()
-    if st in ans:
-        ans[st] += 1
-    else:
-        ans[st] = 1
-print(sum(filter(lambda x: x != 1, ans.values())))
+n, m = map(int, input().split())
+num = 1
+i = j = 0
+start_j = start_i = 0
+sp = list([None] * m for _ in range(n))
+while num <= n * m:
+    sp[i][j] = num
+    num += 1
+    i += 1
+    j -= 1
+    if i >= n or j < 0:
+        if start_j + 1 < m:
+            start_j += 1
+            j = start_j
+            i = 0
+        else:
+            start_i += 1
+            i = start_i
+            j = m - 1
+for i in range(n):
+    for j in range(m):
+        elem = str(sp[i][j])
+        print((3 - len(elem)) * " " + elem, end="")
+    print()
