1777eece71796c28ac4bf03c6d73df380a848cd2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-14 22:07:29 +0300

    Авторизация

--- Stepik/basic data stractures.py
+++ Stepik/basic data stractures.py
@@ -1,24 +1,14 @@
-n, m = map(int, input().split())
-num = 1
-i = j = 0
-start_j = start_i = 0
-sp = list([None] * m for _ in range(n))
-while num <= n * m:
-    sp[i][j] = num
-    num += 1
-    i += 1
-    j -= 1
-    if i >= n or j < 0:
-        if start_j + 1 < m:
-            start_j += 1
-            j = start_j
-            i = 0
-        else:
-            start_i += 1
-            i = start_i
-            j = m - 1
+n = int(input())
+mn = set()
 for i in range(n):
-    for j in range(m):
-        elem = str(sp[i][j])
-        print((3 - len(elem)) * " " + elem, end="")
-    print()
+    st = input()
+    mn.add(st)
+m = int(input())
+for j in range(m):
+    st = input()
+    flag = False
+    for elem in mn:
+        if elem == st or st.startswith(elem):
+            flag = True
+            break
+    print("YES" if flag else "NO")
