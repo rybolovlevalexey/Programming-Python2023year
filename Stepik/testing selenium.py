0a8120a89ccbd7e6514163920031518a8236c596
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-21 12:12:56 +0300

    открытие браузера с расширениями

--- Stepik/testing selenium.py
+++ Stepik/testing selenium.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 
-url = "https://stepik.org/"
-browser = webdriver.Chrome()
-browser.get(url)
+#url = "https://stepik.org/"
+#browser = webdriver.Chrome()
+#browser.get(url)
+
+options_chrome = webdriver.ChromeOptions()
+options_chrome.add_extension("coordinates.crx")
+with webdriver.Chrome(options=options_chrome) as browser:
+    url = "https://yandex.ru/"
+    browser.get(url)
