27cd7b89c7de387a44fc9d092ac232ee9d2ad584
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-23 19:51:25 +0300

    проход по всем собранным ссылкам по первой стартовой ссылке

--- Stepik/основы и применение.py
+++ Stepik/основы и применение.py
@@ -1,20 +1,15 @@
 import requests
-import lxml
-import bs4
 import re
 
-url = input()
-res = requests.get(url)
-res.encoding = "utf-8"
-links = list(elem.get("href")
-           for elem in bs4.BeautifulSoup(res.text, "lxml").find_all("a"))
-links = filter(lambda x: x is not None, links)
-ans = set()
-for line in links:
-    if "://" not in line:
-        continue
-    result = line[line.index("://") + 3:]
-    while "/" in result or ":" in result:
-        result = result[:-1]
-    ans.add(result.strip())
-print(*sorted(ans), sep="\n")
+first = input()
+second = input()
+res = requests.get(first)
+urls = list()
+result_first = re.findall("href=\"(.*)\"", res.text)
+for new_url in result_first:
+    responce = requests.get(new_url)
+    urls.extend(re.findall("href=\"(.*)\"", responce.text))
+if second in urls or second.replace("stepik", "stepic") in urls:
+    print("Yes")
+else:
+    print("No")
