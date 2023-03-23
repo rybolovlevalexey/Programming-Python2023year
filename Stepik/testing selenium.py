d6c862f83a173ed979cb5fb9a2baac1a0b2ddff5
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-23 19:25:31 +0300

    сбор всех ссылок с html файла по его ссылке

--- Stepik/testing selenium.py
+++ Stepik/testing selenium.py
@@ -1,11 +1,14 @@
 from selenium import webdriver
 
-#url = "https://stepik.org/"
-#browser = webdriver.Chrome()
-#browser.get(url)
-
+''' тестирование selenium webdriver
+url = "https://stepik.org/"
+browser = webdriver.Chrome()
+browser.get(url)
+'''
+''' открытие браузера с расширениями
 options_chrome = webdriver.ChromeOptions()
 options_chrome.add_extension("coordinates.crx")
 with webdriver.Chrome(options=options_chrome) as browser:
     url = "https://yandex.ru/"
     browser.get(url)
+'''
--- Stepik/основы и применение.py
+++ Stepik/основы и применение.py
@@ -1,37 +1,20 @@
-# (имя, родители, дети)
-sp_classes = dict()
-n = int(input())
-for i in range(n):
-    stroka = input().strip()
-    if ":" not in stroka:
-        sp_classes[stroka] = set()
-    else:
-        name, parents = stroka.split(" : ")
-        sp_classes[name] = set(parents.split())
-for key in sp_classes.keys():
-    stack = list(sp_classes[key])
-    while len(stack) > 0:
-        par = stack.pop(0)
-        sp_classes[key].add(par)
-        for new_par in sp_classes[par]:
-            if new_par not in stack and new_par not in sp_classes[key]:
-                stack.append(new_par)
-#print(sp_classes)
+import requests
+import lxml
+import bs4
+import re
 
-m = int(input())
-sp = list()
-for i in range(m):
-    error = input()
-    if len(sp) == 0:
-        sp.append(error)
-        for key in sp_classes:
-            if error in sp_classes[key]:
-                sp.append(key)
-    else:
-        if error not in sp:
-            sp.append(error)
-            for key in sp_classes:
-                if error in sp_classes[key]:
-                    sp.append(key)
-        else:
-            print(error)
+url = input()
+res = requests.get(url)
+res.encoding = "utf-8"
+links = list(elem.get("href")
+           for elem in bs4.BeautifulSoup(res.text, "lxml").find_all("a"))
+links = filter(lambda x: x is not None, links)
+ans = set()
+for line in links:
+    if "://" not in line:
+        continue
+    result = line[line.index("://") + 3:]
+    while "/" in result or ":" in result:
+        result = result[:-1]
+    ans.add(result.strip())
+print(*sorted(ans), sep="\n")
