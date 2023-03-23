63a3f86bef86c8869db51c9b93ceb75f1c9a5f7e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-03-23 22:59:31 +0300

    richest customer wealth

--- Stepik/leetcode.py
+++ Stepik/leetcode.py
@@ -1,59 +1,7 @@
-class FizzBuzz(object):
-    def __init__(self, n):
-        self.n = n
-        for num in range(1, n + 1):
-            if num % 3 == 0 and num % 5 == 0:
-                self.fizzbuzz(self.printFizzBuzz)
-            elif num % 3 == 0:
-                self.fizz(self.printFizz)
-            elif num % 5 == 0:
-                self.buzz(self.printBuzz)
-            else:
-                self.number(num, self.prNumber)
-
-    def prNumber(self, x):
-        print(str(x))
-
-    def printFizzBuzz(self):
-        print("fizzbuzz")
-
-    def printBuzz(self):
-        print("buzz")
-
-    def printFizz(self):
-        print("fizz")
-
-    # printFizz() outputs "fizz"
-    def fizz(self, printFizz):
+class Solution(object):
+    def maximumWealth(self, accounts):
         """
-        :type printFizz: method
-        :rtype: void
+        :type accounts: List[List[int]]
+        :rtype: int
         """
-        printFizz()
-
-    # printBuzz() outputs "buzz"
-    def buzz(self, printBuzz):
-        """
-        :type printBuzz: method
-        :rtype: void
-        """
-        printBuzz()
-
-    # printFizzBuzz() outputs "fizzbuzz"
-    def fizzbuzz(self, printFizzBuzz):
-        """
-        :type printFizzBuzz: method
-        :rtype: void
-        """
-        printFizzBuzz()
-
-    # printNumber(x) outputs "x", where x is an integer.
-    def number(self, x, printNumber):
-        """
-        :type printNumber: method
-        :rtype: void
-        """
-        printNumber(x)
-
-
-sl = FizzBuzz(15)
+        return max(map(sum, accounts))
