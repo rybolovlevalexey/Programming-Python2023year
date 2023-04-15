a06447672767e087e7946bff79f38a84435c6d8f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-04-15 12:20:10 +0300

    Космополитизм

--- Yandex Intensive/Отбор на стажировку/D.py
+++ Yandex Intensive/Отбор на стажировку/D.py
@@ -1,21 +1,30 @@
 n = int(input())  # кол-во стран
-can_dohod = list(map(int, input().split()))  # минимальный доход
-can_obraz = list(map(int, input().split()))  # необходимость образования
-can_child = list(map(int, input().split()))  # может ли ребёнок переехать
+can_dohod = map(int, input().split())  # минимальный доход
+can_obraz = map(int, input().split())  # необходимость образования
+can_child = map(int, input().split())  # может ли ребёнок переехать
+country = dict()
+for i in range(1, n + 1):
+    country[i] = (can_dohod.__next__(), can_obraz.__next__(), can_child.__next__())
+mn_doh = min(country.values(), key=lambda x: x[0])[0]
 q = int(input())
 dohod = map(int, input().split())
 obraz = map(int, input().split())
 parents = map(int, input().split())
-for j in range(q):
+output = list()
+for i in range(q):
+    ans = set()
     dj, oj, pj = dohod.__next__(), obraz.__next__(), parents.__next__()
-    ans = list()
-    if pj != 0 and can_child[pj - 1] != 0:
-        ans.append(pj)
-    for i in range(n):
-        if dj >= can_dohod[i] and oj >= can_obraz[i]:
-            ans.append(i + 1)
+    for j in country:
+        if dj == 0 and oj == 0 and pj == 0:
+            if country[j][0] == 0 and country[j][1] == 0 and (country[j][2] == 0 or country[j][2] == 1):
+                ans.add(j)
+                break
+        if ((dj >= country[j][0]) and (oj - country[j][1] >= 0)) or\
+                ((pj == j) and (country[j][2] == 1)):
+            ans.add(j)
             break
     if len(ans) == 0:
-        print(0, end=" ")
+        output.append(0)
     else:
-        print(min(ans), end=" ")
+        output.append(min(ans))
+print(*output)
