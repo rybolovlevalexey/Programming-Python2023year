14c255b1ddf9b968bfed5f7b3033a6a023f938ac
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-05-11 21:58:51 +0300

    юургу

--- Yandex Intensive/Test.py
+++ Yandex Intensive/Test.py
@@ -1,3 +1,15 @@
-sp = list(map(int, input().split()))
-print(sum(sp))
-print(sp[0] * sp[1])
+import random
+
+n = int(input("Введите кол-во строк в массиве "))
+m = int(input("Введите кол-во столбцов в массиве "))
+matrix = list(list(random.randint(-10, 10) for j in range(m)) for i in range(n))
+print(*matrix, sep="\n")
+
+num = int(input("Введите интересующий номер столбца "))
+if num < 1 or num > m:
+    print("Введён неверный номер столбца")
+else:
+    summ = 0
+    for i in range(n):
+        summ += matrix[i][num - 1]
+    print(f"Среднее арифметическое {num}-того столбца: {summ / n}")
