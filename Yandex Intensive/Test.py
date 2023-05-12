37635849bd11edc4cbecd998186e0006aa594d0d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-05-12 21:16:02 +0300

    юургу

--- Yandex Intensive/Test.py
+++ Yandex Intensive/Test.py
@@ -1,26 +1,40 @@
-# 2.2
+#3.2
 import random
 
-sp_x = list()
-sp_y = list()
-# вариант ввода с клавиатуры координат для векторов X и Y
-print("Введите координаты вектора X через пробел")
-for elem in input().split():
-    sp_x.append(int(elem))
-print("Введите координаты вектора Y через пробел")
-for elem in input().split():
-    sp_y.append(int(elem))
-
-# вариант ввода координат для векторов X и Y рандомно из программы
-#n = int(input("Введите размерность указанного векторного пространства "))
+# вариант с ручным вводом матрицы
+n = int(input("Введите кол-во строк матрицы "))
+m = int(input("Введите кол-во столбцов матрицы "))
+matrix = list()
+#print("Введите матрицу")
 #for i in range(n):
-#    sp_x.append(random.randint(-9, 9))
-#    sp_y.append(random.randint(-9, 9))
-#print(f"Вектор X:", *sp_x)
-#print(f"Вектор Y:", *sp_y)
+#    line = list()
+#    for elem in input().split():
+#        line.append(int(elem))
+#    matrix.append(line)
+# вариант с рандомным заданием исходной матрицы
+print("Матрица A:")
+for i in range(n):
+    line = list()
+    for j in range(m):
+        elem = random.randint(-9, 9)
+        line.append(elem)
+        elem = str(elem)
+        if j != 0:
+            print(" " * (4 - len(elem)) + elem, end="")
+        else:
+            print(" " * (2 - len(elem)) + elem, end="")
+    matrix.append(line)
+    print()
 
-sp_z = list()
-for i in range(len(sp_x)):
-    sp_z.append(max(sp_x[i], sp_y[i]))
-print("Координаты вектора Z")
-print(*sp_z)
+print("Матрица B:")
+answer = list()
+for i in range(n):
+    answer.append(sorted(matrix[i])[::-1])
+for line in answer:
+    for j in range(m):
+        elem = str(line[j])
+        if j != 0:
+            print(" " * (4 - len(elem)) + elem, end="")
+        else:
+            print(" " * (2 - len(elem)) + elem, end="")
+    print()
