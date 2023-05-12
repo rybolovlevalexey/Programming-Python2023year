b1f51792a5bd16a241f58351ec1eae285cab9675
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-05-12 20:55:25 +0300

    юургу

--- Yandex Intensive/Test.py
+++ Yandex Intensive/Test.py
@@ -1,15 +1,26 @@
+# 2.2
 import random
 
-n = int(input("Введите кол-во строк в массиве "))
-m = int(input("Введите кол-во столбцов в массиве "))
-matrix = list(list(random.randint(-10, 10) for j in range(m)) for i in range(n))
-print(*matrix, sep="\n")
+sp_x = list()
+sp_y = list()
+# вариант ввода с клавиатуры координат для векторов X и Y
+print("Введите координаты вектора X через пробел")
+for elem in input().split():
+    sp_x.append(int(elem))
+print("Введите координаты вектора Y через пробел")
+for elem in input().split():
+    sp_y.append(int(elem))
 
-num = int(input("Введите интересующий номер столбца "))
-if num < 1 or num > m:
-    print("Введён неверный номер столбца")
-else:
-    summ = 0
-    for i in range(n):
-        summ += matrix[i][num - 1]
-    print(f"Среднее арифметическое {num}-того столбца: {summ / n}")
+# вариант ввода координат для векторов X и Y рандомно из программы
+#n = int(input("Введите размерность указанного векторного пространства "))
+#for i in range(n):
+#    sp_x.append(random.randint(-9, 9))
+#    sp_y.append(random.randint(-9, 9))
+#print(f"Вектор X:", *sp_x)
+#print(f"Вектор Y:", *sp_y)
+
+sp_z = list()
+for i in range(len(sp_x)):
+    sp_z.append(max(sp_x[i], sp_y[i]))
+print("Координаты вектора Z")
+print(*sp_z)
