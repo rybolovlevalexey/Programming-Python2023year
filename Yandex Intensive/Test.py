8d5f3627a27b87a5884919d8109f3701511bf5da
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-05-13 12:02:21 +0300

    юургу 3.2б

--- Yandex Intensive/Test.py
+++ Yandex Intensive/Test.py
@@ -1,40 +1,21 @@
-#3.2
+#3.2б
 import random
 
-# вариант с ручным вводом матрицы
-n = int(input("Введите кол-во строк матрицы "))
-m = int(input("Введите кол-во столбцов матрицы "))
+n = int(input("Введите порядок матрицы "))
 matrix = list()
-#print("Введите матрицу")
-#for i in range(n):
-#    line = list()
-#    for elem in input().split():
-#        line.append(int(elem))
-#    matrix.append(line)
-# вариант с рандомным заданием исходной матрицы
-print("Матрица A:")
 for i in range(n):
     line = list()
-    for j in range(m):
-        elem = random.randint(-9, 9)
-        line.append(elem)
-        elem = str(elem)
-        if j != 0:
-            print(" " * (4 - len(elem)) + elem, end="")
-        else:
-            print(" " * (2 - len(elem)) + elem, end="")
+    for j in range(n):
+        line.append(random.randint(-9, 9))
     matrix.append(line)
-    print()
-
-print("Матрица B:")
-answer = list()
-for i in range(n):
-    answer.append(sorted(matrix[i])[::-1])
-for line in answer:
-    for j in range(m):
-        elem = str(line[j])
-        if j != 0:
-            print(" " * (4 - len(elem)) + elem, end="")
-        else:
+for line in matrix:
+    for i in range(n):
+        elem = str(line[i])
+        if i == 0:
             print(" " * (2 - len(elem)) + elem, end="")
+        else:
+            print(" " * (3 - len(elem)) + elem, end="")
     print()
+for i in range(n):
+    if matrix[i][i] < 0:
+        print(f"Наибольший элемент строки {i + 1}: {max(matrix[i])}")
