f3cf732d6da4d8cc7c4424bdfcfd714f2f00cda1
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-10-29 11:52:36 +0300

    algorithm на этом усё

--- Yandex Cup 2023 qualification/B.py
+++ Yandex Cup 2023 qualification/B.py
@@ -1,13 +1,17 @@
 n, m = map(int, input().split())
 costs = list(map(int, input().split()))
+if m == 0:
+    print(0)
 left, right = 0, n - 1
 target = sorted(costs)[:m]
 counter = {i: costs.count(i) for i in set(costs)}
 for elem in target:
     counter[elem] -= 1
-le_val, ri_val = costs[left], costs[right]
-while counter[le_val] != 0 or counter[ri_val] != 0:
+print(counter)
+while True:
     le_val, ri_val = costs[left], costs[right]
+    if counter[le_val] == 0 and counter[ri_val] == 0:
+        break
     if counter[le_val] > 0:
         left += 1
         counter[le_val] -= 1
@@ -19,4 +23,5 @@ while counter[le_val] != 0 or counter[ri_val] != 0:
     if left == right:
         break
 
+print(counter)
 print(right - left + 1)
