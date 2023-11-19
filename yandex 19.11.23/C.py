52928f482f8dda3ce3a61d9a0b75febad0c0da28
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-11-19 12:59:24 +0300

    TL - test 31 (не помогло вычитать уже из суммированной переменной)

--- yandex 19.11.23/C.py
+++ yandex 19.11.23/C.py
@@ -1,37 +1,37 @@
 n, m, q = map(int, input().split())
 column_names: list[str] = input().split()
 col_dict: dict[str, list[int]] = {key: list() for key in column_names}
-bad_lines: set[int] = set()
-
+# bad_lines: set[int] = set()
+lines_sum = dict()
+result = 0
+minus_lines = set()
 for i in range(n):
     j = 0
     sp = list(map(int, input().split()))
     for key in column_names:
         col_dict[key] += [sp[j]]
         j += 1
+    summa = sum(sp)
+    lines_sum[i] = summa
+    result += summa
 for i in range(q):
     name, qk, value = input().split()
     line_ind = 0
     value = int(value)
-    if len(bad_lines) == n:
+    if len(minus_lines) == n:
         break
     if qk == ">":
         for elem in col_dict[name]:
             if elem <= value:
-                bad_lines.add(line_ind)
+                if line_ind not in minus_lines:
+                    minus_lines.add(line_ind)
+                    result -= lines_sum[line_ind]
             line_ind += 1
     else:
         for elem in col_dict[name]:
             if elem >= value:
-                bad_lines.add(line_ind)
+                if line_ind not in minus_lines:
+                    minus_lines.add(line_ind)
+                    result -= lines_sum[line_ind]
             line_ind += 1
-
-if len(bad_lines) == n:
-    print(0)
-else:
-    result = 0
-    indexes = list(i for i in range(n) if i not in bad_lines)
-    for column in col_dict.values():
-        for i in indexes:
-            result += column[i]
-    print(result)
+print(result)
