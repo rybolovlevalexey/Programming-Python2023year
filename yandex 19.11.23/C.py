cd046480c3456072dab4d2e9c9141f4dda58af58
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-11-19 13:24:51 +0300

    Ok

--- yandex 19.11.23/C.py
+++ yandex 19.11.23/C.py
@@ -1,10 +1,10 @@
 n, m, q = map(int, input().split())
 column_names: list[str] = input().split()
 col_dict: dict[str, list[int]] = {key: list() for key in column_names}
-# bad_lines: set[int] = set()
 lines_sum = dict()
 result = 0
 minus_lines = set()
+actions = dict()
 for i in range(n):
     j = 0
     sp = list(map(int, input().split()))
@@ -16,22 +16,40 @@ for i in range(n):
     result += summa
 for i in range(q):
     name, qk, value = input().split()
-    line_ind = 0
     value = int(value)
+    if name not in actions:
+        actions[name] = dict()
+
+    if qk not in actions[name]:
+        actions[name][qk] = value
+    else:
+        if qk == ">":
+            actions[name][qk] = max(value, actions[name][qk])
+        else:
+            actions[name][qk] = min(value, actions[name][qk])
+
+for name, acts in actions.items():
     if len(minus_lines) == n:
         break
-    if qk == ">":
+    upper = acts.get("<", None)
+    down = acts.get(">", None)
+    line_ind = 0
+    if upper is None:
         for elem in col_dict[name]:
-            if elem <= value:
-                if line_ind not in minus_lines:
-                    minus_lines.add(line_ind)
-                    result -= lines_sum[line_ind]
+            if elem <= down and line_ind not in minus_lines:
+                minus_lines.add(line_ind)
+                result -= lines_sum[line_ind]
+            line_ind += 1
+    elif down is None:
+        for elem in col_dict[name]:
+            if elem >= upper and line_ind not in minus_lines:
+                minus_lines.add(line_ind)
+                result -= lines_sum[line_ind]
             line_ind += 1
     else:
         for elem in col_dict[name]:
-            if elem >= value:
-                if line_ind not in minus_lines:
-                    minus_lines.add(line_ind)
-                    result -= lines_sum[line_ind]
+            if (elem >= upper or elem <= down) and line_ind not in minus_lines:
+                minus_lines.add(line_ind)
+                result -= lines_sum[line_ind]
             line_ind += 1
 print(result)
