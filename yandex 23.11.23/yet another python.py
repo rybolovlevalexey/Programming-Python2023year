39b7ca06bfe7cb8620ab1e4798836041ece0f4d8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-12-06 23:14:38 +0300

    Строительство лесенок

--- yandex 23.11.23/yet another python.py
+++ yandex 23.11.23/yet another python.py
@@ -1,7 +1,6 @@
-import numpy as np
-
-array = np.loadtxt("file_name.txt")
-k = int(input()) - 1
-col_num = list(np.where(array[k] == array[k].min()))[0]
-summa = sum(filter(lambda x: x < 0, array[:, col_num]))
-print(*summa)
+n = int(input())
+line_num = 1
+while line_num <= n:
+    n -= line_num
+    line_num += 1
+print(line_num - 1)
