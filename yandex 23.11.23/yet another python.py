b67b35dc99fa798f23e077de75e97fb4aac4760f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-12-12 19:38:26 +0300

    Размер поддеревьев

--- yandex 23.11.23/yet another python.py
+++ yandex 23.11.23/yet another python.py
@@ -1,22 +1,17 @@
 n = int(input())
-sp: list[tuple[int]] = list()
-for i in range(n):
-    time = tuple(map(int, input().split()))
-    if len(sp) == 0 or time[1] < sp[0][1] or (time[1] == sp[0][1] and time[0] <= sp[0][0]):
-        sp = [time] + sp
-        continue
-    for j in range(1, i - 1):
-        if sp[j - 1][1] < time[1] < sp[j + 1][1] or \
-                (sp[j - 1][1] <= time[1] < sp[j + 1][1] and sp[j - 1][0] <= time[0]) or \
-                (sp[j - 1][1] < time[1] <= sp[j + 1][1] and time[0] <= sp[j + 1][0]):
-            sp.insert(j, time)
-            break
-    else:
-        sp.append(time)
-cur_time = None
-ans = 0
-for elem in sp:
-    if cur_time is None or elem[0] >= cur_time[1]:
-        cur_time = elem
-        ans += 1
-print(ans)
+tree = dict()  # номер узла: (номер предка, список потомков)
+for i in range(n - 1):
+    first, second = (map(int, input().split()))
+    if first not in tree:
+        tree[first] = (None, list())
+    tree[first][1].append(second)
+    if second not in tree:
+        tree[second] = (first, list())
+    cur = tree[first][0]
+    while cur is not None:
+        tree[cur][1].append(second)
+        cur = tree[cur][0]
+ans = list()
+for key in sorted(tree.keys()):
+    ans.append(len(tree[key][1]) + 1)
+print(*ans)
