d9b2a409f185d2a6f5c9fa32ad1c405aec1995cd
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-12-13 16:55:45 +0300

    Путь к файлу

--- yandex 23.11.23/yet another python.py
+++ yandex 23.11.23/yet another python.py
@@ -1,17 +1,23 @@
+name = input()
 n = int(input())
-tree = dict()  # номер узла: (номер предка, список потомков)
-for i in range(n - 1):
-    first, second = (map(int, input().split()))
-    if first not in tree:
-        tree[first] = (None, list())
-    tree[first][1].append(second)
-    if second not in tree:
-        tree[second] = (first, list())
-    cur = tree[first][0]
-    while cur is not None:
-        tree[cur][1].append(second)
-        cur = tree[cur][0]
-ans = list()
-for key in sorted(tree.keys()):
-    ans.append(len(tree[key][1]) + 1)
-print(*ans)
+files = dict()
+cur_path = dict()
+for i in range(n):
+    st = input()
+    j = 0
+    for j in range(len(st)):
+        if st[j] != " ":
+            break
+    if len(cur_path) == 0 or j > max(cur_path.keys()):
+        if "." in st:
+            files[st.strip()] = list(cur_path[key] for key in sorted(cur_path.keys()))
+        else:
+            cur_path[j] = st.strip()
+    else:
+        while j < max(cur_path.keys()):
+            del cur_path[max(cur_path.keys())]
+        if "." in st:
+            files[st.strip()] = list(cur_path.values())
+        else:
+            cur_path[j] = st.strip()
+print(f"/{'/'.join(files[name])}/{name}")
